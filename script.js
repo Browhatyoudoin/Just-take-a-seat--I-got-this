1c24c4eda4d2550afaf0efca40ae3a33b9509c4d
chairpotato <chairpotato09@gmail.com>
2024-09-13 00:12:05 -0700

    help im dying w the json json at hom commit

--- script.js
+++ script.js
@@ -13,11 +13,16 @@ function accessInfo(){
             console.error("Unable to fetch data:", error));
     }
 
-// function revealBox(){
-//     const studentInfo = document.getElementById("studentAdd");
-//     studentInfo.addEventListener('click', function(event) {
-    
-    
-//     }
-//     )
-// }
+function addStudent(){
+    let form = document.getElementById("dataForm");
+    let formData = {};
+    for (let i = 0; i < form.elements.length; i++) {
+        let element = form.elements[i];
+        if (element.type !== "submit") {
+            formData[element.name] = element.value;
+        }
+    }
+    let jsonData = JSON.stringify(formData);
+    let jsonOutput = document.getElementById("jsonOutput");
+    jsonOutput.innerHTML = "<pre>" + jsonData + "</pre>";
+}

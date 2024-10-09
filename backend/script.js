bf24e62f5b5d4a2cceb424716eb2346a0c581d16
Browhatyoudoin <jessnuts999@gmail.com>
2024-10-09 14:31:32 -0700

    added desks and desk dragging not working yet :C

--- backend/script.js
+++ backend/script.js
@@ -37,6 +37,38 @@ function addStudent(){
     console.log(accessInfo());
 
     
-   
     
+}
+
+dragElement(document.getElementById("desk"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    elmnt.addEventListener("mousedown", dragMouseDown);
+
+    function dragMouseDown(e) {
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.addEventListener("mouseup", closeDragElement);
+        document.addEventListener("mousemove", elementDrag);
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.removeEventListener("mouseup", closeDragElement);
+        document.removeEventListener("mousemove", elementDrag);
+    }
 }

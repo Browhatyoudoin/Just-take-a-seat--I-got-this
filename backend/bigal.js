48b15f0eb6f591019ef2798eaf1ce4ddcc0a18ba
Browhatyoudoin <jessnuts999@gmail.com>
2024-10-28 21:51:56 -0700

    algorithm theory crafting

---
+++ backend/bigal.js
@@ -0,0 +1,6 @@
+//access student info based on what was inputted into addStudent.html
+//creates total#ofstudents/3 groups and numbers them
+//makes 2 different arrays:
+//1. list of students
+//2. 2D array --> outer array corresponds to the # of students, inner array lists the numbers of previous groups
+//
